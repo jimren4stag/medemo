213e1c81725516763c65d5edca634000429d98d1
Jimm <jimren@stagwell.tech>
2023-11-20 15:41:43 -0500

    test

---
+++ src/lib/sleepapp/header.svelte
@@ -0,0 +1,41 @@
+<header>
+  <div class="logo">
+    <h1>sleepapp</h1>
+  </div>
+
+  <div class="navbar">
+    <ul class="nav">
+      <li><a href="/">home</a></li>
+      <li><a href="/">tests</a></li>
+      <li><a href="/">about</a></li>
+      <li><a href="/">login</a></li>
+    </ul>
+  </div>
+</header>
+
+<style>
+  header {
+    background-color: black;
+    padding: 16px;
+  }
+  .navbar {
+    display:flex;
+    justify-content: flex-end;
+  }
+  ul.nav {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  ul.nav li {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  ul.nav li a {
+    text-decoration: none;
+    padding: 5px 10px;
+    border-radius: 5px;
+    color: default;
+  }
+  
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,8 @@
+<script>
+  import "$lib/sleepapp/global.css";
+  import Header from "$lib/sleepapp/header.svelte";
+  import Footer from "$lib/sleepapp/footer.svelte";
+</script>
 
 <Header></Header>
 <slot />
